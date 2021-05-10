55ddfd51d3474fe31b679a855b33352e40abfac0
nokyy <nokyy@protonmail.com>
2021-05-10 18:18:02 +0200

    wyklad 3

---
+++ src/services/movies_service.ts
@@ -0,0 +1,38 @@
+import http from '../utilities/http';
+import {omdApiKey} from '../key';
+const url = 'http://www.omdbapi.com/';
+const movieService = {
+    searchByName: async (name: string, page: number) =>{
+        try{
+            const response = await <any>http.get(url, {
+                apikey: omdApiKey,
+                s: name,
+                page
+            });
+            if(response?.Error){
+                console.log(response.Error);
+                return null;
+            } else {
+                return{
+                    totalResults: parseInt(response.totalResults, 10),
+                    movies: response.Search.map((movie: any)=>({
+                        id: movie.imdbID,
+                        poster: movie.Poster,
+                        title: movie.Title,
+                        type: movie.Type,
+                        year: movie.Year,
+                    }))
+                }
+            }
+        }catch(error){
+            console.log(error);
+        }
+    },
+    getById: async (id: string) =>{
+        const response = await <any>http.get(url, {
+            apikey: omdApiKey,
+            i: id,
+        });
+    }
+};
+export default movieService;
--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import NavPanel from '../../components/NavPanel';
+import {omdApiKey} from '../../key'
+import movieService from '../../services/movies_service';
 
 const Home = () =>{
+    const [search, setSearch] = React.useState('harry potter');
+    const [movie, setMovie] = React.useState({});
+
+    React.useEffect(() => {
+        const makeRequest = async () => {
+            try{
+                const response = movieService.searchByName(search, 1);
+                setMovie(response);
+            } catch(error){
+            console.log(error);
+        }
+    };
+        makeRequest();
+    },[search]);
+
     return(<div>
         <NavPanel/>
         <div>This is home page.</div>
